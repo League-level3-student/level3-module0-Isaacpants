6a6ab54dc79ef9a5ca82ee7c25b060b8522da6f9
Isaacpants <dave@boyermoving.com>
2020-04-07 16:11:51 -0700

    finished hidden button

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -4,63 +4,65 @@ import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		String arr[] = {"abc","df","g", "h", "i"};
-		
+		// 1. make an array of 5 Strings
+		String arr[] = { "abc", "df", "g", "h", "i" };
+
 		String[] arr2 = new String[5];
-		arr2[0] ="apple";
-		arr[1]="pear";
-		//2. print the third element in the array
-System.out.println(arr[2]);
-		//3. set the third element to a different value
-arr[2] = "abcdefghi";
-		//4. print the third element again
-System.out.println(arr[2]);
-		//5. use a for loop to set all the elements in the array to a string of your choice
+		arr2[0] = "apple";
+		arr[1] = "pear";
+		// 2. print the third element in the array
+		System.out.println(arr[2]);
+		// 3. set the third element to a different value
+		arr[2] = "abcdefghi";
+		// 4. print the third element again
+		System.out.println(arr[2]);
+		// 5. use a for loop to set all the elements in the array to a string of your
+		// choice
 		for (int i = 0; i < arr.length; i++) {
-			arr[i]= "r";
+			arr[i] = "r";
 		}
-		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		// 6. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
-		
+
 		}
-		//7. make an array of 50 integers
-int[] num = new int[50];
-		//8. use a for loop to make every value of the integer array a random number
-for (int i = 0; i < num.length; i++) {
-	Random rand = new Random();
-	num[i] = rand.nextInt(50);
-}
-		//9. without printing the entire array, print only the smallest number on the array
-int smallest = 10;
-for (int i = 0; i < num.length; i++) {
-	if(i<49) {
-	if(smallest>num[i]) {
-		smallest = num[i];
-	}
-	}
-}
-System.out.println(""+ smallest);
+		// 7. make an array of 50 integers
+		int[] num = new int[50];
+		// 8. use a for loop to make every value of the integer array a random number
+		for (int i = 0; i < num.length; i++) {
+			Random rand = new Random();
+			num[i] = rand.nextInt(50);
+		}
+		// 9. without printing the entire array, print only the smallest number on the
+		// array
+		int smallest = 10;
+		for (int i = 0; i < num.length; i++) {
+			if (i < 49) {
+				if (smallest > num[i]) {
+					smallest = num[i];
+				}
+			}
+		}
+		System.out.println("" + smallest);
 
-		//10 print the entire array to see if step 8 was correct
-for (int i = 0; i < num.length; i++) {
-	System.out.println(num[i]);
-}
+		// 10 print the entire array to see if step 8 was correct
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}
 
-		//11. print the largest number in the array.
-int largest = 10;
-for (int i = 0; i < num.length; i++) {
-	if(i<49) {
-	if(largest<num[i]) {
-		largest = num[i];
-	}
-	}
-}
-System.out.println(""+ largest);
+		// 11. print the largest number in the array.
+		int largest = 10;
+		for (int i = 0; i < num.length; i++) {
+			if (i < 49) {
+				if (largest < num[i]) {
+					largest = num[i];
+				}
+			}
+		}
+		System.out.println("" + largest);
 
-		//12. print only the last element in the array
+		// 12. print only the last element in the array
 		System.out.println(num[49]);
 	}
 }
--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -3,7 +3,7 @@ package _00_IntroToArrays;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-	
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -11,61 +11,89 @@ import javax.swing.JPanel;
 
 /* Run the FindHiddenButton.jar to see the finished product. */
 
-public class _02_FindTheHiddenButton implements ActionListener{
+public class _02_FindTheHiddenButton implements ActionListener {
 	JFrame window;
 	JPanel panel;
-	
-	//1. create an array of JButtons. Don't initialize it yet.
+
+	// 1. create an array of JButtons. Don't initialize it yet.
 	JButton[] jb;
-	//2 create an integer variable called hiddenButton
+	// 2 create an integer variable called hiddenButton
 	int hiddenButton;
+
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
-	
+
 	public void start() {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//3.  Ask the user to enter a positive number and convert it to an int
-		String num = JOptionPane.showInputDialog("Give me a number");
-		int number = Integer.parseInt(num);
-		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		hiddenButton = number;
-		//5. Make a for loop to iterate through the JButton array
-		for (int i = 0; i < jb.length; i++) {
-			//6. initialize each JButton in the array
-			
-			//7. add the ActionListener to each JButton
-			jb[i].addActionListener(this);
-			//8. add each JButton to the panel
-			jb[i].add(panel);
-	}
-		//9 add the panel to the window
-		window.add(panel);
-		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH);  
-		//11. set the JFrame to visible.
-		window.setVisible(true);
-		//12. Give the user the instructions for the game.
-		JOptionPane.showMessageDialog(null, "click ");
-		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
-		//14. Set the text of the JButton located at hiddenButton to read "ME"
 
-		//15. Use Thread.sleep(100); to pause the program.
+		// 3. Ask the user to enter a positive number and convert it to an int
+		System.out.println("hi");
+		String num = JOptionPane.showInputDialog("Give me a number");
+
+		int number = Integer.parseInt(num);
+
+		// 4. Initialize the array of JButtons to be the size of the int created in step
+		// 3
+		jb = new JButton[number];
+		// 5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < jb.length; i++) {
+			// 6. initialize each JButton in the array
+			jb[i] = new JButton();
+			// 7. add the ActionListener to each JButton
+			jb[i].addActionListener(this);
+			// 8. add each JButton to the panel
+			panel.add(jb[i]);
+			System.out.println("hello");
+		}
+		// 9 add the panel to the window
+		window.add(panel);
+		// 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// 11. set the JFrame to visible.
+		window.setVisible(true);
+		// 12. Give the user the instructions for the game.
+		JOptionPane.showMessageDialog(null, "click on the correct button");
+		// 13. initialize the hiddenButton variable to a random number less than the int
+		// created in step 3
+		Random rand = new Random(number);
+		hiddenButton = rand.nextInt(number);
 		
-		//16. Set the text of the JButton located at hiddenButton to be blank.
+		// 14. Set the text of the JButton located at hiddenButton to read "ME"
+
+		jb[hiddenButton].setText("me");
+		// 15. Use Thread.sleep(100); to pause the program.
+
+		 try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// 16. Set the text of the JButton located at hiddenButton to be blank.
+	
+			 jb[hiddenButton].setText("");
+
 		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton buttonClicked = (JButton)e.getSource();
-		
-		//17. if the hiddenButton is clicked, tell the user that they win.
+		JButton buttonClicked = (JButton) e.getSource();
 
-		//18. else tell them to try again
+		// 17. if the hiddenButton is clicked, tell the user that they win.
+
+		if (e.getSource() == jb[hiddenButton]) {
+			JOptionPane.showMessageDialog(null, "You Win");
+		} else {
+			// 18. else tell them to try again
+			JOptionPane.showMessageDialog(null, "try again");
+
+		}
+
 	}
 }
